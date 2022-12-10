84516b92f8e38de536eca711bf29e7f2e8fcc2e1
Kyle Coffey <me@phytolizer.dev>
2022-12-10 02:08:49 -0600

    main cleanup
    del input
    
    ignore input

--- build.zig
+++ build.zig
@@ -3,8 +3,10 @@ const std = @import("std");
 const DAYS_IMPLEMENTED = 4;
 
 const RenderArgs = struct {
-    day: []const u8,
-    daynum: []const u8,
+    day: []const u8 = "",
+    daynum: []const u8 = "",
+    daynumfmt: []const u8 = "",
+    implnum: []const u8 = std.fmt.comptimePrint("{d}", .{DAYS_IMPLEMENTED}),
     kind: []const u8 = "",
     part: []const u8 = "",
 };
@@ -27,6 +29,10 @@ fn renderTemplate(template: []const u8, comptime W: type, w: W, args: RenderArgs
             args.day
         else if (std.mem.eql(u8, var_name, "daynum"))
             args.daynum
+        else if (std.mem.eql(u8, var_name, "daynumfmt"))
+            args.daynumfmt
+        else if (std.mem.eql(u8, var_name, "implnum"))
+            args.implnum
         else if (std.mem.eql(u8, var_name, "kind"))
             args.kind
         else if (std.mem.eql(u8, var_name, "part"))
@@ -49,6 +55,82 @@ pub fn build(b: *std.build.Builder) !void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
+    var day_nums: [DAYS_IMPLEMENTED]usize = undefined;
+    {
+        var day_num: usize = 0;
+        while (day_num < DAYS_IMPLEMENTED) : (day_num += 1) {
+            day_nums[day_num] = day_num + 1;
+        }
+    }
+    var days: [DAYS_IMPLEMENTED][]u8 = undefined;
+    for (day_nums) |day| {
+        days[day - 1] = try std.fmt.allocPrint(b.allocator, "{d}", .{day});
+    }
+    var days_fmt: [DAYS_IMPLEMENTED][]u8 = undefined;
+    for (day_nums) |day| {
+        days_fmt[day - 1] = try std.fmt.allocPrint(b.allocator, "{d:0>2}", .{day});
+    }
+    var day_names: [DAYS_IMPLEMENTED][]u8 = undefined;
+    for (day_nums) |day| {
+        day_names[day - 1] = try std.fmt.allocPrint(b.allocator, "dec{s}", .{days_fmt[day - 1]});
+    }
+    defer for (day_nums) |day| {
+        b.allocator.free(days[day - 1]);
+        b.allocator.free(day_names[day - 1]);
+    };
+
+    const header = @embedFile("templates/header.template");
+    const import_day = @embedFile("templates/import_day.template");
+    const input = @embedFile("templates/input.template");
+    const run_day = @embedFile("templates/run_day.template");
+    const run_particular_day = @embedFile("templates/run_particular_day.template");
+    const run_day_end = @embedFile("templates/run_day_end.template");
+    const kinds = &[_][]const u8{ "simple", "full" };
+
+    var all_input_f = try std.fs.cwd().createFile("src/all_input.zig", .{});
+    var all_input = all_input_f.writer();
+
+    for (day_nums) |day| {
+        for (kinds) |kind| {
+            try renderTemplate(
+                input,
+                @TypeOf(all_input),
+                all_input,
+                .{
+                    .day = day_names[day - 1],
+                    .daynumfmt = days_fmt[day - 1],
+                    .kind = kind,
+                },
+            );
+        }
+    }
+
+    var all_days_f = try std.fs.cwd().createFile("src/all_days.zig", .{});
+    var all_days = all_days_f.writer();
+
+    try all_days.writeAll(header);
+    for (day_nums) |day| {
+        try renderTemplate(
+            import_day,
+            @TypeOf(all_days),
+            all_days,
+            .{ .day = day_names[day - 1], .daynum = days[day - 1] },
+        );
+    }
+    try renderTemplate(run_day, @TypeOf(all_days), all_days, .{});
+
+    for (day_nums) |day| {
+        try renderTemplate(
+            run_particular_day,
+            @TypeOf(all_days),
+            all_days,
+            .{ .day = day_names[day - 1], .daynum = days[day - 1] },
+        );
+    }
+
+    try all_days.writeAll(run_day_end);
+    all_days_f.close();
+
     const exe = b.addExecutable("aoc-2022", "src/main.zig");
     exe.setBuildMode(mode);
     exe.install();
@@ -58,28 +140,23 @@ pub fn build(b: *std.build.Builder) !void {
 
     const test_step = b.step("test", "Run tests");
 
-    const day_template = @embedFile("templates/advent_test.zig.template");
     const fragment = @embedFile("templates/test_fragment.template");
 
     const amalgamated_test_path = "src/test.zig";
     const amalgamated_test = try std.fs.cwd().createFile(amalgamated_test_path, .{});
     var writer = amalgamated_test.writer();
-    try writer.writeAll(@embedFile("templates/header.template"));
+    try writer.writeAll(header);
 
-    var day_num: usize = 0;
-    while (day_num < DAYS_IMPLEMENTED) : (day_num += 1) {
-        const day = try std.fmt.allocPrint(b.allocator, "{d:0>2}", .{day_num + 1});
-        const day_name = try std.fmt.allocPrint(b.allocator, "dec{s}", .{day});
+    for (day_nums) |day_num| {
         {
             try renderTemplate(
-                day_template,
+                import_day,
                 @TypeOf(writer),
                 writer,
-                .{ .day = day_name, .daynum = day },
+                .{ .day = day_names[day_num - 1], .daynum = days[day_num - 1] },
             );
 
             const parts = &[_][]const u8{ "1", "2" };
-            const kinds = &[_][]const u8{ "simple", "full" };
 
             for (parts) |part| {
                 for (kinds) |kind| {
@@ -88,8 +165,8 @@ pub fn build(b: *std.build.Builder) !void {
                         @TypeOf(writer),
                         writer,
                         .{
-                            .day = day_name,
-                            .daynum = day,
+                            .day = day_names[day_num - 1],
+                            .daynum = days[day_num - 1],
                             .kind = kind,
                             .part = part,
                         },
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,21 @@
 const std = @import("std");
+const all_days = @import("all_days.zig");
 
-pub fn main() void {
-    std.debug.print("hi\n", .{});
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.detectLeaks();
+    const a = gpa.allocator();
+
+    const args = try std.process.argsAlloc(a);
+    defer std.process.argsFree(a, args);
+
+    if (args.len > 1) {
+        const day = try std.fmt.parseInt(u8, args[1], 10);
+        if (day > 0 and day <= all_days.IMPLEMENTED_DAYS) {
+            try all_days.run_day(a, day);
+        } else {
+            std.debug.print("Day {d} is not implemented yet\n", .{day});
+            std.process.exit(1);
+        }
+    }
 }
